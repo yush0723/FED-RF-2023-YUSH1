b2a62acac71a9baa7a16b3b0ef8b22e2424afec1
yush0723 <yush0723@gmail.com>
2023-09-01 16:20:26 +0900

    2

--- 002.CSS학습/05.3DAni/05.3DAni/js/cube2.js
+++ 002.CSS학습/05.3DAni/05.3DAni/js/cube2.js
@@ -1,4 +1,4 @@
-// 큐브 애니 JS //////////////
+// 큐브 애니2 JS - cube2.js //////////////
 
 /************************************* 
     [구현내용]
@@ -11,10 +11,10 @@
 
 *************************************/
 
-// window 로드 이벤트 호출
-window.addEventListener('DOMContentLoaded', loadFn)
+// window 로드 이벤트 호출 ///
+window.addEventListener('DOMContentLoaded',loadFn);
 
-// DOM 선택함수
+// DOM 선택함수 //////
 const qs = x => document.querySelector(x);
 const qsa = x => document.querySelectorAll(x);
 
@@ -23,36 +23,8 @@ function loadFn(){
     // 함수호출확인
     console.log('로딩완료!');
 
-    // 1. 대상선정: .btngo
-    const btngo = qs('.btngo');
-    // 1-2. 변경대상: .cube
-    const cube = qs('.cube');
-    console.log('대상:', btngo, cube);
+    /// 부드러운 스크롤 호출
+    startSS();
 
-    // 2. 이벤트 함수 설정하기
-    btngo.onclick = e => {
-        // 호출확인
-        console.log('돌아!');
-        // 1. 대상: .cube -> cube변수
-        // 2. 변경내용 :
-        // 클래스on 없으면 넣고 있으면 빼기
-        // classList.toggle(클래스명)
-        cube.classList.toggle('on');
-    // 3. 텍스트 읽어오기(버튼글자 변경위해)
-    let btxt = e.target.innerText;
-    console.log("화살표 함수에서 this:",this,
-    "화살표함수에서 e.target",e.target,
-    "현재버튼글자:",btxt);
-
-    // 4. 버튼글자 변경하기: "돌아" <-> "멈춰"
-    e.target.innerText = 
-    btxt=='돌아!'?'멈춰!':"돌아!";
-    // 할당할 데이터를 삼항연산자로 결정!
-    // 비?집:놀이동산
-    }; //// click이벤트 함수 ////
-
-}////// loadFn함수 /////////
-////////////////////////////
-
-
-
+} /////////////// loadFn함수 //////////////
+///////////////////////////////////////////
