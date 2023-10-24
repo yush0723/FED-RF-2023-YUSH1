6c36a87f76408fabadb4733a8f525b6015f5e0bd
yush0723 <yush0723@gmail.com>
2023-10-24 12:45:05 +0900

    20231024-1

--- 003.JS학습/js/js9-3.local_storage.js
+++ 003.JS학습/js/js9-3.local_storage.js
@@ -79,6 +79,7 @@ function localSFn(){
     // 1. 버튼 텍스트 읽기
     let btxt = this.innerText;
     console.log('로컬쓰:',btxt);
+
     // 2. 버튼별 기능 분기하기 ////
     if(btxt == '처음'){
         // 로컬 스토리지 읽기 : 
@@ -92,6 +93,10 @@ function localSFn(){
         localStorage.setItem('lname','이정재');
         localStorage.setItem('lrole','박평호역');
         localStorage.setItem('lcat','조직내 스파이를 색출하는 해외팀 안기부팀장');
+
+        // 로컬스토리지.key(순번) -> 키이름을 리턴함!
+        console.log('두번째(1) 키명은?',localStorage.key(1),
+        '\n전체개수:',localStorage.length);
         
         // console.log('로컬쓰 lname:',
         // localStorage.getItem('lname'));
@@ -101,6 +106,7 @@ function localSFn(){
         // 해당 url로 관리되는 로컬쓰를 모두 지움! : clear()
         localStorage.clear();
         // 개별 로컬쓰로 지우는 방법은 removeItem(키명)
+        bindData();
     } /////// else if : 전체삭제 ////////////
     else if(btxt == '보여줘'){
         dFn.qs('.local .nm').innerText = 
@@ -114,29 +120,224 @@ function localSFn(){
 
     // -> 객체를 생성하여 로컬 스토리지에 넣기
     else if(btxt = '처리'){
-        // 게시판 형식의 객체를 생성함!
-        let obj = [
-            {
-                idx: 1,
-                tit: '내가 왕이될 상인가?',
-                cont: '이정재형은 진정 왕이십니다~!',
-            },
-        ];
+        // 로컬쓰에 'minfo'가 없으면 makeObj() 호출!
+        if(!localStorage.getItem('minfo')) makeObj();
 
-        // 로컬 스토리지에 넣기
-        // 배열/객체를 직접 넣으면 데이터형 표시 문자값만
-        // 입력되고 실제 객체는 입력되지 않는다!
-        // -> 왜??? 로컬스토리지는 문자형만 받으니까!
-        // 그래서 배열/객체를 문자데이터화 해서 넣는다!!!
-        // JSON.stringify(배열/객체)
-        // localStorage.setItem('minfo',obj);
-        localStorage.setItem('minfo',JSON.stringify(obj));
-
+        // 바인딩 함수 호출!
+        bindData();
+        
     } //////// else if : 처리 ////////////////
-
-
-
+    
+    
+    
 } //////////// localSFn 함수 ////////////////
+
+// 처음에 바인딩 함수 호출하여 게시판 보이기 ////
+bindData();
+
+/// 객체가 없으면 로컬스토리지에 생성하기 ///////
+function makeObj(){  
+    console.log('배열/객체만들기!!!');  
+
+    // 게시판 형식의 객체를 생성함!
+    let obj = [
+        {
+            idx: 1,
+            tit: '내가 왕이될 상인가?',
+            cont: '이정재형은 진정 왕이십니다~!',
+        },
+    ];
+    
+    // 로컬 스토리지에 넣기
+    // 배열/객체를 직접 넣으면 데이터형 표시 문자값만
+    // 입력되고 실제 객체는 입력되지 않는다!
+    // -> 왜??? 로컬스토리지는 문자형만 받으니까!
+    // 그래서 배열/객체를 문자데이터화 해서 넣는다!!!
+    // JSON.stringify(배열/객체)
+    // localStorage.setItem('minfo',obj);
+    localStorage.setItem('minfo',JSON.stringify(obj));
+} /////////////// makeObj 함수 ///////////////
+
+
+/// 화면에 게시판 리스트를 데이터에 맞게 바인딩하기 /////
+function bindData(){
+
+    // 1. 로컬스토리지 데이터 : 문자형(string)
+    let localData = localStorage.getItem('minfo');
+    console.log(localData,"데이터형:",typeof localData);
+
+    
+    // 바인딩 데이터변수
+    let bindCode = '';
+    
+    // 2. 데이터 존재 여부 확인하기
+    if(localData){ // null이 아니면 true!
+        // 문자형을 배열로 형변환해야함!!!
+        // 로컬스토리지 데이터 배열객체형변환 
+        // -> JSON.parse(문자형배열객체)
+        localData = JSON.parse(localData);
+        console.log(localData,
+            "데이터형:", typeof localData, 
+            "배열인가? ",Array.isArray(localData));
+        
+        // 배열이니까 map()사용하여 태그만들기!
+        // -> 맵쬬잉~!!!! map().join('')
+        bindCode = localData.map((v,i)=>`
+            <tr>
+                <td>${v.idx}</td>
+                <td>${v.tit}</td>
+                <td>${v.cont}</td>
+                <td class="del-link">
+                    <a href="#" data-idx="${i}">×</a>
+                </td>
+            </tr>
+        `).join(''); // 태그를 연결자없는 배열전체로 저장
+    } ///////// if : 데이터가 있는 경우 ///////
+    else{ // 데이터가 없는 경우 //////////
+        bindCode = `
+            <tr>
+                <td colspan="4" style="text-align:center">
+                    데이터가 없습니다...
+                </td>
+            </tr>
+        `;
+
+    } /////////////// else //////////////
+
+
+    // 3. 화면에 테이블 요소로 데이터바인딩 구성하기
+    let hcode = `
+        <table>
+            <tr>
+                <th>번호</th>
+                <th>제목</th>
+                <th>내용</th>
+                <th>삭제</th>
+            </tr>
+            <!-- 데이터에 따른 반복바인딩 -->
+            ${bindCode}
+        </table>
+    `;
+
+    // 4. 화면출력 : 대상 - .board
+    dFn.qs('.board').innerHTML = hcode;
+
+    // 5. 화면출력 후 지우기 링크 셋팅하기
+    dFn.qsa('.board .del-link a')
+    .forEach(ele=>dFn.addEvt(ele,'click',
+    ()=>delRec(ele.getAttribute('data-idx'))));
+
+} /////////////// bindData 함수 ////////////////
+
+/// 입력 처리함수 호출 이벤트설정하기 ///////
+dFn.addEvt(dFn.qs('#sbtn'),'click',insData);
+
+/// 입력 처리함수 /////////////////////
+function insData(){
+    // 1. 입력항목 읽어오기
+    let tit = dFn.qs('#tit').value;
+    let cont = dFn.qs('#cont').value;
+    
+    // 2. 만약 하나라도 비었다면 돌아가!
+    // trim() 앞뒤공백제거 -> 스페이스바만 쳐도 불통과!
+    if(tit.trim()=="" || cont.trim()==""){
+        alert('입력데이터가 없습니다! 모두 입력하세요!');
+        return;
+    } //////////// if /////////////
+
+    
+    // 3. 입력처리하기
+    // 3-1.로컬쓰 데이터 가져오기 : minfo
+    let orgData = localStorage.getItem('minfo');
+
+    // 만약 minfo 로컬쓰가 null이면 빈 배열로 생성하기!
+    if(!orgData){
+        // 빈 배열로 생성하기
+        localStorage.setItem('minfo','[]');
+        // 초기 로컬쓰 재할당!
+        orgData = localStorage.getItem('minfo');
+    } ////////// if /////////////
+
+    // 3-2.제이슨 파싱!
+    orgData = JSON.parse(orgData);
+
+    // 3-3. 자동증가번호 처리하기
+    // 배열을 오름차순으로 정렬하여 맨끝 배열데이터의 idx값을
+    // 읽어온후 숫자화 하여 +1처리함!
+    // 3-3-1. 배열 오름차순처리 : 
+    // -> 배열.sort((a,b)=>{
+    // return a == b ? 0 : a > b ? 1 : -1})
+    // -> 배열.sort((a,b)=>{
+    // return a.idx == b.idx ? 0 : a.idx > b.idx ? 1 : -1})
+
+    // 배열값이 있을때만 정렬적용!
+    if(orgData.length != 0){
+        orgData.sort((a,b)=>{
+            return a.idx == b.idx ? 
+            0 : a.idx > b.idx ? 1 : -1
+        }); ///// sort /////
+    }
+
+    // 3-3-2. idx값으로 마지막배열값 읽기
+    let lastArr = orgData.length==0?
+     0 : orgData[orgData.length-1].idx;
+
+    console.log('정렬결과:',orgData,'\n마지막idx값:',lastArr);
+
+
+    // 3-4.입력된 데이터 추가하기 : 배열 push() 메서드
+    // 자동 증가번호는 배열개수+1
+    orgData.push({
+        'idx': lastArr+1,
+        'tit':tit,
+        'cont':cont
+    }); //////// push ///////
+
+    // 3-4. 배열/객체 데이터를 문자화하여 로컬쓰에 넣기
+    // JSON.stringify()
+    localStorage.setItem('minfo',
+    JSON.stringify(orgData));
+
+    console.log('입력처리함~!!',orgData);
+
+    // 4. 리스트 업데이트하기
+    bindData();   
+
+
+} ///////////// insData 함수 //////////////////
+
+/// 삭제 처리함수 /////////////////////
+function delRec(idx){
+    console.log('지울순번:',idx);
+    // 1.a요소 기본이동막기
+    event.preventDefault();
+
+    // 2. 로컬쓰 가져오기
+    // 2-1.로컬쓰 데이터 가져오기 : minfo
+    let orgData = localStorage.getItem('minfo');
+    // 2-2.제이슨 파싱!
+    orgData = JSON.parse(orgData);
+
+    // 3. 특정 데이터 배열항목 삭제
+    // splice(순번,개수) -> 특정순번부터 몇개지움
+    // 여기서는 1개삭제 이므로 splice(순번,1)
+    // confirm(메시지)
+    // -> 확인,취소 중 확인 클릭시 true리턴함!(취소는 false)
+    if(confirm('정말정말정말정말로 지우시게요?????')){
+        orgData.splice(idx,1);
+        console.log('제거후배열:',orgData);
+
+        // 4. 배열/객체 데이터를 문자화하여 로컬쓰에 넣기
+        // JSON.stringify()
+        localStorage.setItem('minfo',
+        JSON.stringify(orgData));
+
+        // 5. 리스트 업데이트하기
+        bindData();   
+    } ////////// if ///////////////
+
+
+} ////////// delRec함수 //////////////////
 
 
 
