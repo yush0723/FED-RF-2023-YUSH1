ff0170363eea8c3fdebca1f5093068f9f159a13e
yush0723 <yush0723@gmail.com>
2023-12-07 10:32:02 +0900

    1

--- 700.쉐도잉프로젝트/04.tt_PJ/yush-app/my-app/src/App.js
+++ 700.쉐도잉프로젝트/04.tt_PJ/yush-app/my-app/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isrc = 'https://image.cine21.com/resize/cine21/person/2019/1030/11_35_56__5db8f70c422b9[X252,310].jpg'; 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={isrc} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
