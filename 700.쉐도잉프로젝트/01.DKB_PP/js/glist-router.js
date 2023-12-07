6e2aef03cd51b10ff22359a9217b66b96745cde1
yush0723 <yush0723@gmail.com>
2023-12-08 04:13:05 +0900

    2

--- 700.쉐도잉프로젝트/01.DKB_PP/js/glist-router.js
+++ 700.쉐도잉프로젝트/01.DKB_PP/js/glist-router.js
@@ -3,7 +3,7 @@
 
 
 // 라우터 템플릿 만들기
-let Glist = {
+let Glist1 = {
     template: `
         <section>
             <!-- 필터옵션 체크박스구역 -->
@@ -70,139 +70,138 @@ let Glist = {
 };
 
 
-// v-model 디렉티브 속성은 요소 자신의 값이
-// 타겟 값으로 반영되게해준다!
-// 변경 데이터의 뷰JS의 동기화기능을 확인!
-
-// v-for에 기술된 v-if조건에 사이범위를 넣고 스토어변수로 컨트롤한다!
-// 조건: 인덱스번호가 1이상 10이하 -> 여기에 일정수를 더함
-// 10씩 더하면 다음 범위가 리스트로 정해짐!!
-// v.idx >= 1 + $store.state.pnum && 
-//  v.idx <= 10 + $store.state.pnum
-let Paging = {
+// 라우터 템플릿 만들기
+let Glist2 = {
     template: `
-    <section>
-        <!-- 상품리스트박스 -->
-        <div class="grid">
-            <div 
-            v-for="
-                (v,i) in $store.state.gdata
-            "
-            v-if="
-                v.idx >= 1 + $store.state.pnum && 
-                v.idx <= 10 + $store.state.pnum
-            ">
+        <section>
+            <!-- 필터옵션 체크박스구역 -->
+            <div id="optbx">
+                <label for="men">a</label>
+                <input type="checkbox" id="men"
+                v-model="$store.state.chkarr[0]"
+                @change="$store.commit('resCheck')">
 
-               
-            <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
-            <router-link 
-                v-bind:to="{
-                    name:'det',
-                    params:{id : v.idx, list : $route.path}
-                }">
-                [{{v.idx}}]
-                
-                <img 
-                    v-bind:src="
-                    './images/goods/'+
-                    v.cat +
-                    '/'+v.ginfo[0]+'.png'  
-                    " alt="dress" />
-                <aside>
-                    <h2>{{v.ginfo[1]}}</h2>
-                    <h3>{{v.ginfo[3]}}</h3>
-                </aside>
+                <label for="women">b</label>
+                <input type="checkbox" id="women"
+                v-model="$store.state.chkarr[1]"
+                @change="$store.commit('resCheck')">
 
-            </router-link>
+                <label for="style">c</label>
+                <input type="checkbox" id="style"
+                v-model="$store.state.chkarr[2]"
+                @change="$store.commit('resCheck')">
 
             </div>
-        </div>
 
-        <!-- 페이징 표시구역 -->
-        <div id="paging">
-            <a href="#" 
-            @click.prevent="
-            $store.commit('updatePaging',0)">
-                1
-            </a>
-             | 
-            <a href="#" 
-            @click.prevent="
-            $store.commit('updatePaging',10)">
-                2
-            </a>
-             | 
-            <a href="#" 
-            @click.prevent="
-            $store.commit('updatePaging',20)">
-                3
-            </a>
-            | 
-            <a href="#" 
-            @click.prevent="
-            $store.commit('updatePaging',30)">
-                4
-            </a>
-            | 
-            <a href="#" 
-            @click.prevent="
-            $store.commit('updatePaging',40)">
-                5
-            </a>
-        </div>
-    </section>
-`,
-};
-let More = {
-    template: `
-    <section>
-        <!-- 상품리스트박스 -->
-        <div class="grid">
-            <div 
-            v-for="
-                (v,i) in $store.state.gdata
-            "
-            v-if="
-                v.idx >= 1 && 
-                v.idx <= 5 + $store.state.mnum
-            ">
+            <!-- 상품리스트박스 -->
+            <div class="grid">
+                <div 
+                    v-for="
+                        (v,i) in $store.state.gdata
+                    "
+                    v-if="
+                        v.cat==$store.state.selnm[0] ||
+                        v.cat==$store.state.selnm[1] ||
+                        v.cat==$store.state.selnm[2]                 
+                    "
+                    @click="$store.commit('setBtn')"
+                
+                >
 
-               
-            <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
-            <router-link 
-                v-bind:to="{
-                    name:'det',
-                    params:{id : v.idx, list : $route.path}
-                }">
+                
+                <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
+                <router-link 
+                    v-bind:to="{
+                        name:'det',
+                        params:{id : v.idx, list : $route.path}
+                    }">
+                [{{v.idx}}]
+                    <img 
+                        v-bind:src="
+                        './images/goods/'+
+                        v.cat +
+                        '/'+v.ginfo[0]+'.png'  
+                        " alt="dress" />
+                    <aside>
+                        <h2>{{v.ginfo[1]}}</h2>
+                        <h3>{{v.ginfo[3]}}</h3>
+                    </aside>
+
+
+                </router-link>
+
                     
-                [{{v.idx}}]
-                
-                <img 
-                    v-bind:src="
-                    './images/goods/'+
-                    v.cat +
-                    '/'+v.ginfo[0]+'.png'  
-                    " alt="dress" />
-                <aside>
-                    <h2>{{v.ginfo[1]}}</h2>
-                    <h3>{{v.ginfo[3]}}</h3>
-                </aside>
-            </router-link>
+                </div>
+            </div>
+        </section>
+    `,
+};
+
+// 라우터 템플릿 만들기
+let Glist3 = {
+    template: `
+        <section>
+            <!-- 필터옵션 체크박스구역 -->
+            <div id="optbx">
+                <label for="men">관광지</label>
+                <input type="checkbox" id="men"
+                v-model="$store.state.chkarr[0]"
+                @change="$store.commit('resCheck')">
+
+                <label for="women">음식점</label>
+                <input type="checkbox" id="women"
+                v-model="$store.state.chkarr[1]"
+                @change="$store.commit('resCheck')">
+
+                <label for="style">카페</label>
+                <input type="checkbox" id="style"
+                v-model="$store.state.chkarr[2]"
+                @change="$store.commit('resCheck')">
 
             </div>
-        </div>
 
-        <!-- 모어버튼 표시구역 -->
-        <div id="more" v-if="$store.state.mbtn">
-            <button class="more" 
-            @click.prevent="
-            $store.commit('updateMore',5)"
-            >
-                MORE
-            </button>
-        </div>
-    </section>
-`,
+            <!-- 상품리스트박스 -->
+            <div class="grid">
+                <div 
+                    v-for="
+                        (v,i) in $store.state.gdata
+                    "
+                    v-if="
+                        v.cat==$store.state.selnm[0] ||
+                        v.cat==$store.state.selnm[1] ||
+                        v.cat==$store.state.selnm[2]                 
+                    "
+                    @click="$store.commit('setBtn')"
+                
+                >
+
+                
+                <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
+                <router-link 
+                    v-bind:to="{
+                        name:'det',
+                        params:{id : v.idx, list : $route.path}
+                    }">
+                [{{v.idx}}]
+                    <img 
+                        v-bind:src="
+                        './images/goods/'+
+                        v.cat +
+                        '/'+v.ginfo[0]+'.png'  
+                        " alt="dress" />
+                    <aside>
+                        <h2>{{v.ginfo[1]}}</h2>
+                        <h3>{{v.ginfo[3]}}</h3>
+                    </aside>
+
+
+                </router-link>
+
+                    
+                </div>
+            </div>
+        </section>
+    `,
 };
 
 // 상세보기 구성 템플릿
