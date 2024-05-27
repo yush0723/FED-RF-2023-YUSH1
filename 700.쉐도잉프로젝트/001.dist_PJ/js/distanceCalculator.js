89d4a9362c96722045057a277b30e5352af08d9b
yush0723 <yush0723@gmail.com>
2024-05-27 09:43:15 +0900

    240527-1

--- 700.쉐도잉프로젝트/001.dist_PJ/js/distanceCalculator.js
+++ 700.쉐도잉프로젝트/001.dist_PJ/js/distanceCalculator.js
@@ -5,44 +5,43 @@ const bannerData = [
         name: 'Barcelona',
         lat: 41.3879169,
         lon: 2.1699187,
-        img: './images/barc.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Barcelona_view_from_Montjuic_3.jpg'
     },
     {
         id: 2,
         name: 'Madrid',
         lat: 40.4167413,
         lon: -3.7032498,
-        img: './images/madr.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Madrid_skyline.jpg'
     },
     {
         id: 3,
         name: 'New York',
         lat: 40.712776,
         lon: -74.005974,
-        img: './images/newy.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/New_York_City_skyline.jpg'
     },
     {
         id: 4,
         name: 'Los Angeles',
         lat: 34.052235,
         lon: -118.243683,
-        img: './images/losa.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Los_Angeles_skyline.jpg'
     },
     {
         id: 5,
         name: 'Sydney',
         lat: -33.868820,
         lon: 151.209290,
-        img: './images/sydn.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Sydney_skyline.jpg'
     },
     {
         id: 6,
         name: 'Melbourne',
         lat: -37.813629,
         lon: 144.963058,
-        img: './images/melb.JPG'
+        img: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Melbourne_skyline.jpg'
     }
-    // 필요한 만큼 데이터 추가 가능
 ];
 
 function calcularDistancia(lat1, lon1, lat2, lon2) {
@@ -102,4 +101,4 @@ function openDistanceWindow(baseItem) {
 }
 
 // 페이지 로드 시 배너 데이터 표시
-9window.onload = displayBannerData;
+window.onload = displayBannerData;
